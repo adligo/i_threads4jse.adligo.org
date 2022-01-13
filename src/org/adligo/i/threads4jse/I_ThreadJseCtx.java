5c5ce80d161a1f1d96a7a5c612f339d2b64262a1
Scott Morgan <scott@adligo.com>
2022-01-12 19:03:06 -0600

    Refactored this interface, I decided I don't like interface singletons, they have no protection and would be easy to mis use.

--- src/org/adligo/i/threads4jse/I_ThreadJseCtx.java
+++ src/org/adligo/i/threads4jse/I_ThreadJseCtx.java
@@ -9,60 +9,61 @@ import java.util.concurrent.ThreadFactory;
 import org.adligo.i.threads.I_ThreadCtx;
 
 /**
- * This interface contains adaptor methods for basic threading used by JSE 
- *    Like all of Adligo's Ctx (Context) classes this provides a way to 
- * stub out the creation of various methods using mockito_ext.adligo.org.
- *  
+ * This interface contains adaptor methods for basic threading used by JSE Like
+ * all of Adligo's Ctx (Context) classes this provides a way to stub out the
+ * creation of various methods using mockito_ext.adligo.org.
+ * 
  * @author scott
  *
  * 
- *  ---------------- Apache ICENSE-2.0 --------------------------
+ *         ---------------- Apache ICENSE-2.0 --------------------------
  *
- * Copyright 2022 Adligo Inc
+ *         Copyright 2022 Adligo Inc
  * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ *         Licensed under the Apache License, Version 2.0 (the "License"); you
+ *         may not use this file except in compliance with the License. You may
+ *         obtain a copy of the License at
  * 
- *    http://www.apache.org/licenses/LICENSE-2.0
+ *         http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ *         Unless required by applicable law or agreed to in writing, software
+ *         distributed under the License is distributed on an "AS IS" BASIS,
+ *         WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ *         implied. See the License for the specific language governing
+ *         permissions and limitations under the License.
  */
 public interface I_ThreadJseCtx extends I_ThreadCtx {
-
 
   /**
    * This should generally return the value from;
    * 
    * https://download.java.net/java/early_access/loom/docs/api/java.base/java/lang/Runtime.html#availableProcessors()
+   * 
    * @return
    * @see {@link java.lang.Runtime#availableProcessors()}
    */
-  public int availableProcessors();
+  default int availableProcessors() {
+    return Runtime.getRuntime().availableProcessors();
+  }
+
   /**
-   * This will eventually return new VirtualThreads when Project Loom's
-   * Virtual Thread are promoted from pre-release to actually released;
+   * This will eventually return new VirtualThreads when Project Loom's Virtual
+   * Thread are promoted from pre-release to actually released;
    * https://download.java.net/java/early_access/loom/docs/api/java.base/java/util/concurrent/Executors.html#newVirtualThreadPerTaskExecutor()
    * <br/>
    * <br/>
-   * However until then it SHOULD return a workStealingPool that is 8X the number of availableProcessors();
-   * Of course custom implementations will very.
-   * <br/>
+   * However until then it SHOULD return a workStealingPool that is 8X the number
+   * of availableProcessors(); Of course custom implementations will very. <br/>
    * <br/>
    * https://download.java.net/java/early_access/loom/docs/api/java.base/java/util/concurrent/Executors.html#newWorkStealingPool(int)
    * <br/>
    * <br/>
-   * https://www.youtube.com/watch?v=EO9oMiL1fFo
-   * No more thread Pools ? async await is 'thread like' but not a real thread
+   * https://www.youtube.com/watch?v=EO9oMiL1fFo No more thread Pools ? async
+   * await is 'thread like' but not a real thread
    * https://download.java.net/java/early_access/loom/docs/api/java.base/java/util/concurrent/Executors.html#newVirtualThreadPerTaskExecutor()
-   * Throughput optimization formula 
+   * Throughput optimization formula
    * 
-   * Brian Gotez 'Reactive Coding is dead'
-   * records are a surrogate for Tuples
+   * Brian Gotez 'Reactive Coding is dead' records are a surrogate for Tuples
    * https://www.youtube.com/watch?v=9si7gK94gLo
    * 
    * @return either a good old fashioned Java Thread or a Virtual Thread
@@ -70,40 +71,54 @@ public interface I_ThreadJseCtx extends I_ThreadCtx {
    * @see I_ThreadJseCtx#availableProcessors()
    */
   public Executor getDefaultExecutor();
-  
+
   /**
-   * Wraps the {@link Executors#newScheduledThreadPool(int)} 
+   * Wraps the {@link Executors#newScheduledThreadPool(int)}
+   * 
    * @return
    * @see Executors#newScheduledThreadPool(int)
    */
-  public ScheduledExecutorService newScheduledThreadPool(int i);
+  default ScheduledExecutorService newScheduledThreadPool(int i) {
+    return Executors.newScheduledThreadPool(i);
+  }
 
   /**
-   * Wraps the {@link Executors#newScheduledThreadPool(int, ThreadFactory)} 
+   * Wraps the {@link Executors#newScheduledThreadPool(int, ThreadFactory)}
+   * 
    * @return
    * @see Executors#newScheduledThreadPool(int, ThreadFactory)
    */
-  public ScheduledExecutorService newScheduledThreadPool(int i, ThreadFactory threadFactory);
-
+  default ScheduledExecutorService newScheduledThreadPool(int i, ThreadFactory threadFactory) {
+    return Executors.newScheduledThreadPool(i, threadFactory);
+  }
 
   /**
-   * Wraps the {@link Executors#newSingleThreadScheduledExecutor} 
+   * Wraps the {@link Executors#newSingleThreadScheduledExecutor}
+   * 
    * @return
    * @see Executors#newSingleThreadScheduledExecutor
    */
-  public ScheduledExecutorService newSingleThreadScheduledExecutor();
-  
+  default ScheduledExecutorService newSingleThreadScheduledExecutor() {
+    return Executors.newSingleThreadScheduledExecutor();
+  }
+
   /**
-   * Wraps the {@link Executors#newSingleThreadScheduledExecutor(ThreadFactory)} 
+   * Wraps the {@link Executors#newSingleThreadScheduledExecutor(ThreadFactory)}
+   * 
    * @return
    * @see Executors#newSingleThreadScheduledExecutor(ThreadFactory)
    */
-  public ScheduledExecutorService newSingleThreadScheduledExecutor(ThreadFactory threadFactory);
+  default ScheduledExecutorService newSingleThreadScheduledExecutor(ThreadFactory threadFactory) {
+    return Executors.newSingleThreadScheduledExecutor(threadFactory);
+  }
 
   /**
    * Wraps the {@link Executors#newWorkStealingPool(int)
+   * 
    * @return
    * @see Executors#newWorkStealingPool(int)
    */
-  public ExecutorService newWorkStealingPool(int parallelism);
+  default ExecutorService newWorkStealingPool(int parallelism) {
+    return Executors.newWorkStealingPool(parallelism);
+  }
 }
